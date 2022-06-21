5d19b8d8f2a8c0f2ecd40b927ee0dc6794bc5100
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-21 16:23:04 +0900

    added routes

--- backend/server.js
+++ backend/server.js
@@ -4,4 +4,6 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use("/api/goals", require("./routes/goalRoutes"));
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
