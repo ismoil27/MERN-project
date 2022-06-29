2eb4614a3545aee26fb6d753b298820bb47ccf52
MrIsmoiljon <abduraimovismoil98@gmail.com>
2022-06-29 13:53:09 +0900

    .

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,8 +7,10 @@ const {
   deleteGoal,
 } = require("../controllers/goalController");
 
-router.route("/").get(getGoals).post(setGoal);
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+const { protect } = require("../middleware/authMiddleware");
+
+router.route("/").get(protect, getGoals).post(protect, setGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 // These are the same routes as above. But if we have same routes we can set our router as above
 // router.get("/", getGoals);
